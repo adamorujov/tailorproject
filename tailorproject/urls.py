94f64bd1bf289c99c911b75e9b0f15e33ff63735
adamorujov <ademorujov@gmail.com>
2023-07-17 01:15:41 +0400

    account api urls added

--- tailorproject/urls.py
+++ tailorproject/urls.py
@@ -23,7 +23,8 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('tailor.api.urls')),
+    path('api/tailor/', include('tailor.api.urls')),
+    path('api/account/', include('account.api.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
